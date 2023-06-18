6a79c4aaefa9ec5503fef026a20c6d7629b84340
Syeda Bokhari <noshaymaano@gmail.com>
2023-06-18 17:20:32 +0000

    soorting and filter

---
+++ filterproduct.js
@@ -0,0 +1,37 @@
+let products = [
+    {
+       name: 'Adidas Alphabounce',
+       price: 5000
+    }, 
+    {
+       name: 'Sandisk USB',
+       price: 300
+    },
+    {
+       name: 'Adidas Ultraboost',
+       price: 8000
+    },
+    {
+       name: 'Samsung LED TV',
+       price: 50000
+    },
+    {
+       name: 'Cadbury 5 star',
+       price: 10
+    } 
+   ]
+
+  function printArray(array){
+      
+    for (let item of array){
+       console.log(item)
+    }
+  }
+
+  function filterByPrice(product){
+    return product.price > 3000;
+  }
+
+  result = products.filter(filterByPrice);
+
+  printArray(result)
---
+++ sorting.js
@@ -0,0 +1,35 @@
+let products = [
+    {
+       name: 'Adidas Alphabounce',
+       price: 5000
+    }, 
+    {
+       name: 'Sandisk USB',
+       price: 300
+    },
+    {
+       name: 'Adidas Ultraboost',
+       price: 8000
+    },
+    {
+       name: 'Samsung LED TV',
+       price: 50000
+    },
+    {
+       name: 'Cadbury 5 star',
+       price: 10
+    } 
+]
+function printArray(array){
+     
+    for (let item of array){
+       console.log(item)
+    }
+ }
+ function sortOnPrice(product1, product2) {
+    return product1.price - product2.price
+ }
+
+ products.sort(sortOnPrice)
+
+ printArray(products)
